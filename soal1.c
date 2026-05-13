769c171eb2d8a25c5abb676ded9e66f6ae02c4fa
Starlightt-ken <kennypramanik2@gmail.com>
2026-05-13 09:35:53 +0700

    Update soal1.c -jawaban1

--- soal1.c
+++ soal1.c
@@ -1,5 +1,49 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 05 - Foundation of Algorithm
+ *   Hari dan Tanggal    : Rabu, 13 Mei 2026
+ *   Nama (NIM)          : Kenny Pramanik Najla Wibowo (133224088)
+ *   Nama File           : modul5.c
+ *   Deskripsi           :
+*/
+#include <stdio.h> 
 
-int main(void) {
+#define MAX 100
+void combination(int pos, int n, int open, int close);
+
+void combination_main(int n){
+    if (n>0){
+        combination(0,n,0,0);
+    }
+    return;
+}
+void combination(int pos, int n, int open, int close){
+    static char str[MAX];
+
+    if(close == n){
+        printf("%s \n", str);
+        return;
+    }
+
+    else{
+
+        if(open < n){
+            str[pos] = '(';
+            combination(pos+1, n, open+1, close);
+        }
+        if (open > close){
+            str[pos] = ')';
+            combination(pos+1, n, open, close+1);
+        }
+    }
+}
+
+int main(){
+    int n; 
+    scanf("%d", &n);
+    
+    combination_main(n);
+    getchar();
     return 0;
+
+
 }
